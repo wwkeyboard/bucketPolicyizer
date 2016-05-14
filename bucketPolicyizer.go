41ad0abc319c90d05e76138e579e143847be0bfb
Aaron Lee <wwkeyboard@gmail.com>
2016-05-14 07:30:11 -0500

    Compile a simple policy to JSON

--- bucketPolicyizer.go
+++ bucketPolicyizer.go
@@ -1,10 +1,25 @@
 package bucketPolicyizer
 
+import "encoding/json"
+
 var defaultVersion = "2012-10-17"
 
-// Policy represents the Bucket policy
+// GetObjectAction the s3 action for getting an object
+var GetObjectAction = "s3:GetObject"
+
+// Policy is the Bucket policy
 type Policy struct {
-	Version string
+	Version   string
+	Statement []Statement
+}
+
+// Statement is a single permission
+type Statement struct {
+	Sid       string
+	Effect    string
+	Principal string
+	Action    []string
+	Resource  []string
 }
 
 // EmptyPolicy creates a valid empty policy
@@ -12,4 +27,15 @@ func EmptyPolicy() Policy {
 	return Policy{
 		Version: defaultVersion,
 	}
+}
+
+// CompilePolicy renders the policy to JSON
+func CompilePolicy(policy Policy) (string, error) {
+	p, err := json.Marshal(policy)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(p), nil
 }
--- bucketPolicyizer_test.go
+++ bucketPolicyizer_test.go
@@ -1,11 +1,44 @@
 package bucketPolicyizer
 
-import "testing"
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+// Many of these test cases are taken from
+// http://docs.aws.amazon.com/AmazonS3/latest/dev/example-bucket-policies.html
 
 func TestVersion(t *testing.T) {
 	policy := EmptyPolicy()
 
 	if policy.Version != "2012-10-17" {
 		t.Error("version not set correctly")
+	}
+}
+
+func TestReadOnlyFromAnonymous(t *testing.T) {
+	policy := EmptyPolicy()
+	action := GetObjectAction
+	resource := "arn:aws:s3::exampleBucket/*"
+	s := Statement{
+		Sid:       "AddCannedAcl",
+		Effect:    "Allow",
+		Principal: "*",
+		Action:    []string{action},
+		Resource:  []string{resource},
+	}
+
+	policy.Statement = []Statement{s}
+
+	p, err := CompilePolicy(policy)
+	if err != nil {
+		t.Error("couldn't compile policy", err)
+	}
+
+	sidTest := regexp.MustCompile(`AddCannedAcl`)
+	if !sidTest.MatchString(p) {
+		fmt.Println(p)
+		t.Error("couldn't match Sid")
 	}
 }
